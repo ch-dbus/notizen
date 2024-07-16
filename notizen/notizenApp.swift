f9ff161e7d86efad4ca31b9f32035176e78ba726
Dominik Bussinger <d.bussinger@gmail.com>
2024-07-16 10:29:00 +0200

    Initial Commit

---
+++ notizen/notizenApp.swift
@@ -0,0 +1,17 @@
+//
+//  notizenApp.swift
+//  notizen
+//
+//  Created by Dominik Bussinger on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct notizenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
